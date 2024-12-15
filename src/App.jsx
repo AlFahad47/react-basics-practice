6a37cf8a25ad831b66c707bded7e71ca592a00a1
MD Fahad Molla <ahfahad47sss@gmail.com>
2024-12-15 18:30:55 +0600

    user usergreeting file conditional rendering done

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import Footer from './Footer'
 import Food from './Food'
 import Card from './Card'
 import Student from './Student'
+import UserGreeting from './UserGreeting'
+import List from './List'
 function App() {
   
   return (
     <>
-    <Header/>
+    {/* <Header/>
     <Food/>
     <Card/>
     <Student age={18} isStudent={true} name="Fahad Molla" />
     <Student age="25" isStudent={false} name="Abdul kalam" />
     <Student age={12} isStudent={true} name="Hero Alom" />
     <Student/>
-    <Footer/> 
+    <Footer/>  */}
+    <UserGreeting isLoggedIn={"true"} username="Fahad Molla" />
+    
     </>
   )
 }
