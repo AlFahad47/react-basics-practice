bad313bab3f0d96f56a69268b891cdba22ab540f
Fahad Molla <ahfahad47sss@gmail.com>
2024-12-16 18:24:07 +0600

    list redering done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ import Student from './Student'
 import UserGreeting from './UserGreeting'
 import List from './List'
 function App() {
+  const fruits = [
+    {id:1,name:"Banana",calories:192},
+    {id:2,name:"Apple",calories:160},
+    {id:3,name:"Orange",calories:102},
+    {id:4,name:"Pineapple",calories:148},
+  ];
   
   return (
     <>
@@ -18,9 +24,10 @@ function App() {
     <Student/>
     <Footer/>  */}
     <UserGreeting isLoggedIn={"true"} username="Fahad Molla" />
+    <List  items={fruits} category="fruit"/>
     
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/List.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const List = (props) => {
+  const itemList = props.items;
+  const category= props.category;
+  const listItems = itemList.map(item=>
+                                    <li key={item.id}>{item.name} &nbsp; <b>{item.calories}</b></li>
+                                )
+  return (
+    <div>
+      <h2 className='list-category'>{category}</h2>
+      <ol className='list-items'> {listItems}</ol>
+    </div>
+  )
+}
+
+export default List
